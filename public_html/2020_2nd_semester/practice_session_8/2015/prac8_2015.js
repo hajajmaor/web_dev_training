f399987c200a56fc58e095177466e5af88f6d8ee
kono <hajajmaor@gmail.com>
2021-04-28 21:45:08 +0300

    practice 8 2015 session

---
+++ public_html/2020_2nd_semester/practice_session_8/2015/prac8_2015.js
@@ -0,0 +1,65 @@
+// EX1
+// alert("Hello class");
+
+// EX2
+// var theUserName = window.prompt("What is you name?");
+// // theUserName <- null
+// console.log("hello, " + theUserName);
+// document.write("hello, " + theUserName)
+
+// EX3
+/*
+var num1, num2, operator;
+num1 = parseInt(window.prompt("enter 1st number:"));
+num2 = parseInt(window.prompt("enter 2nd number:"));
+operator = window.prompt("enter operator: (+,-,*,/)");
+switch (operator) {
+    case "+":
+        document.write(num1 + " " + operator + " " + num2 + " = " + (num1 + num2));
+        break;
+    case "-":
+        document.write(`${num1} ${operator} ${num2} = ${num1 - num2}`);
+        break;
+    case "*":
+        document.write(`${num1} ${operator} ${num2} = ${num1 * num2}`);
+        break;
+    case "/":
+        document.write(`${num1} ${operator} ${num2} = ${num1 / num2}`);
+        break;
+    default:
+        break;
+}
+// if (operator == "+") {
+
+// }
+// else if (operator == '-') { }
+
+*/
+//EX4
+/*
+var num = 1;
+while (num <= 30)
+    document.write(`${num++} ,`);
+*/
+// document.write(`
+// <h1 style="color:red;"> Hello red H1</h1>
+// `);
+// document.write(`
+// <div class="row">
+// <div class="column">
+//     <h6>${title}</h6>
+//     <p>
+//        ${desc}
+//     </p>
+// </div>
+// <img src="${image_src}" alt="" srcset="">
+// </div>
+// `);
+
+//EX5
+/*
+var num = 1;
+while (num <= 30)
+    document.write(`${num++} <br>`);
+*/
+
